762bec02e38acb465a32c6cebadf64ae335f7357
shir koren <102361148+lusilus@users.noreply.github.com>
2022-10-13 20:00:11 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -3,8 +3,8 @@ import * as contentful from 'contentful'
 //here the API key will be:
 
 export const client = contentful.createClient({
-    space:'0jopynzyimt3',
-    accessToken:'dUYNS0MP3TPpKunp35T_zUaH7a-GV71AIKUTfsUpmRA'
+    space:'',
+    accessToken:''
 })
 
 // process.env.REACT_APP_SPACE_ID,
